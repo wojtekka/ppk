154be872b9b3d476b8b264a937e1afb8a8a4fc0e
Wojtek Kaniewski <wojtekka@toxygen.net>
2024-11-14 20:52:28 +0100

    Use assigned USB pid/vid

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
 #pragma once
 
 // USB device descriptor
-#define USB_VENDOR_ID       0x6666      // VID
-#define USB_PRODUCT_ID      0x7777      // PID
+#define USB_VENDOR_ID       0x1209      // VID
+#define USB_PRODUCT_ID      0x2982      // PID
 #define USB_DEVICE_VERSION  0x0100      // v1.0 (BCD-format)
 
 // USB configuration descriptor
